ab200b124415fcb6e4c5955fa72560210d4dc7aa
ken17yama <ken17yama@gmail.com>
2020-05-15 11:54:21 +0900

    'groupListの条件式変更'

--- src/main/java/com/yamada/springboot/app/controller/GroupController.java
+++ src/main/java/com/yamada/springboot/app/controller/GroupController.java
@@ -24,59 +24,56 @@ import com.yamada.springboot.domain.service.UserService;
 
 @Controller
 public class GroupController {
-	
+
 	@Autowired
 	GroupService groupService;
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@GetMapping("/admin/group")
 	public String getAdminGroupList(Model model) {
 		model.addAttribute("contents", "login/groupList :: groupList_contents");
-		
+
 		List<Group> groupList = groupService.selectMany();
-		
+
 		System.out.println(groupList);
-		
+
 		model.addAttribute("groupList", groupList);
-		
+
 		return "login/homeLayout";
 	}
-	
+
 	@GetMapping("/group")
 	public String getGroupList(Model model, Principal principal) {
+		model.addAttribute("contents", "login/groupList :: groupList_contents");
+		
 		//	ユーザーの情報をusersテーブルから取り出し、対象のplaceがあるかを確認
 		String mail = principal.getName();
 		User user = userService.selectOne(mail);
-		List<String> myPlaceStrList = new ArrayList<>(Arrays.asList(user.getMyPlace().split(",")));	
-		List<Integer> myPlaceIntList = myPlaceStrList.stream().map(e -> Integer.valueOf(e)).collect(Collectors.toList());
-		
-		System.out.println(myPlaceIntList);
-		
-		model.addAttribute("contents", "login/groupList :: groupList_contents");
-		
-		List<Group> groupList = groupService.selectThat(myPlaceIntList);
+		if(user.getMyPlace() != null) {
+			List<String> myPlaceStrList = new ArrayList<>(Arrays.asList(user.getMyPlace().split(",")));	
+			List<Integer> myPlaceIntList = myPlaceStrList.stream().map(e -> Integer.valueOf(e)).collect(Collectors.toList());
+			List<Group> groupList = groupService.selectThat(myPlaceIntList);
 
-		System.out.println(groupList);
-		
-		model.addAttribute("groupList", groupList);
-		
+			model.addAttribute("groupList", groupList);
+		}
+
 		return "login/homeLayout";
 	}
-	
+
 	@GetMapping("/group/add")
 	public String getGroupAdd(@ModelAttribute GroupForm form, Model model) {
-		
+
 		model.addAttribute("contents", "login/groupAdd :: groupAdd_contents");
-		
+
 		return "login/homeLayout";
-		
+
 	}
-	
+
 	@PostMapping("/group/add")
 	public String PostAddStock(@ModelAttribute GroupForm form, Model model, Principal principal) {
-		
+
 		System.out.println("追加ボタンの処理");
 
 		Group group = new Group();
@@ -87,39 +84,39 @@ public class GroupController {
 		group.setUpdatedAt(timestamp);
 
 		try {
-				Integer place = groupService.insertOneReturn(group);
-						
-				System.out.println("追加成功");
-				
-				List<String> mailList = form.getGroupList();
-				if(!mailList.contains(principal.getName())) {
-					mailList.add(principal.getName());
+			Integer place = groupService.insertOneReturn(group);
+
+			System.out.println("追加成功");
+
+			List<String> mailList = form.getGroupList();
+			if(!mailList.contains(principal.getName())) {
+				mailList.add(principal.getName());
+			}
+
+			for(String mail : mailList){
+				User user = userService.selectOne(mail);
+				List<String> myPlaceStrList = new ArrayList<>(Arrays.asList(user.getMyPlace().split(",")));
+				List<Integer> myPlaceIntList = myPlaceStrList.stream().map(e -> Integer.valueOf(e)).collect(Collectors.toList());
+				myPlaceIntList.add(place);
+
+				myPlaceStrList = myPlaceIntList.stream().map(e -> String.valueOf(e)).collect(Collectors.toList());
+
+				String myPlaceStr = String.join(",", myPlaceStrList);
+
+				boolean result = userService.editPlace(mail, myPlaceStr);
+				if(result == true) {
+					System.out.println("更新成功");
+				}else {
+					System.out.println("更新失敗");
 				}
-				
-				for(String mail : mailList){
-					User user = userService.selectOne(mail);
-					List<String> myPlaceStrList = new ArrayList<>(Arrays.asList(user.getMyPlace().split(",")));
-					List<Integer> myPlaceIntList = myPlaceStrList.stream().map(e -> Integer.valueOf(e)).collect(Collectors.toList());
-					myPlaceIntList.add(place);
-					
-					myPlaceStrList = myPlaceIntList.stream().map(e -> String.valueOf(e)).collect(Collectors.toList());
-					
-				    String myPlaceStr = String.join(",", myPlaceStrList);
-					
-					boolean result = userService.editPlace(mail, myPlaceStr);
-					if(result == true) {
-						System.out.println("更新成功");
-					}else {
-						System.out.println("更新失敗");
-					}
-				}
+			}
 		}catch(DataAccessException e) {
 			System.out.println("追加失敗（トランザクションテスト）");
 		}
 
 		return "redirect:/group";
 	}
-	
+
 	@GetMapping("/group/{place:.+}/stock/delete")
 	public String getGroupList(Model model, @PathVariable("place") Integer place, Principal principal) {
 		//	ユーザーの情報をusersテーブルから取り出し、対象のplaceがあるかを確認
@@ -127,14 +124,14 @@ public class GroupController {
 		User user = userService.selectOne(mail);
 		List<String> myPlaceStrList = new ArrayList<>(Arrays.asList(user.getMyPlace().split(",")));	
 		List<Integer> myPlaceIntList = myPlaceStrList.stream().map(e -> Integer.valueOf(e)).collect(Collectors.toList());
-//		myPlaceの配列内から「place」に該当するIDがあるか確認
+		//		myPlaceの配列内から「place」に該当するIDがあるか確認
 		if(myPlaceIntList.contains(place)) {
 			System.out.println("OK");
 		} else {
 			System.out.println("NG");
 			return "redirect:/";
 		}
-		
+
 		try {
 			boolean result = groupService.deleteOne(place);
 			if(result == true) {
@@ -145,8 +142,8 @@ public class GroupController {
 		}catch(DataAccessException e) {
 			System.out.println("追加失敗（トランザクションテスト）");
 		}
-		
-		
+
+
 		return "redirect:/group";
 	}
 
